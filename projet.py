93f57359c26b070d5df76b30d719ffba615bce48
MaxenceRogerieux <71320557+MaxenceRogerieux@users.noreply.github.com>
2023-06-01 09:43:26 +0200

    Update projet.py
    adding comments

--- projet.py
+++ projet.py
@@ -5,6 +5,13 @@ import pandas as pd
 import pylab as plt
 import seaborn as sns
 
+# instant : date du séisme
+# lat : latitude du séisme
+# long : longitude du séisme
+# profondeur : profondeur du séisme
+# mag : magnitude du séisme
+# pays : région du séisme
+
 #Lecture du fichier csv
 df = pd.read_csv('seismes_2014(1).csv', sep=';')
 
@@ -18,6 +25,3 @@ plt.show()
 #Affichage des données avec seaborn
 sns.regplot(x='x', y='y', data=df)
 plt.show()
-
-
-
