95492b70274d2d48d216979989cd7c13e5311870
Mael-GN <mael.grelneau@gmail.com>
2023-06-01 09:12:37 +0200

    Inport start files

--- projet.py
+++ projet.py
@@ -1,1 +1,23 @@
-print "hello world"
+#Lire un fichier csv et afficher les données avec pandas, pylab et seabornù
+
+
+import pandas as pd
+import pylab as plt
+import seaborn as sns
+
+#Lecture du fichier csv
+df = pd.read_csv('seismes_2014(1).csv', sep=';')
+
+#Affichage des données
+print(df)
+
+#Affichage des données avec pylab
+plt.plot(df['x'], df['y'], 'ro')
+plt.show()
+
+#Affichage des données avec seaborn
+sns.regplot(x='x', y='y', data=df)
+plt.show()
+
+
+
