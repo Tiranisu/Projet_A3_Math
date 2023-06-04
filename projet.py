41c0b0ddb402091e6f64a9298a8b50b7b52d266b
MaxenceRogerieux <71320557+MaxenceRogerieux@users.noreply.github.com>
2023-06-04 19:46:39 +0200

    finish part3
    cleaning comments and adding hover text on map

--- projet.py
+++ projet.py
@@ -147,49 +147,11 @@ E = pd.DataFrame()
 
 E['effectif'] = F['m'].value_counts()
 
-E['values'] = E.values
-
 E['index'] = E.index
-
-E['effectif'] = E.values/len(F)*100
 
 E = E.sort_values(by='index',ascending = True)
 
-print("Effectif de chaque magnitude :")
-print(E)
-print("FIN")
-
 #-------------------- Q2 ----------------------
-
-# figM = px.density_mapbox(F[F['m'] >= 7], lat="lat", lon="lon", hover_name="pays", hover_data=["mag"], zoom=1, radius=10)
-
-H = df[df['mag'] >= 3]
-H['m'] = H['mag'].astype(int)
-
-H['size'] = 5 + 2*(H['m']-2)
-H = H.sort_values(by='m',ascending = True)
-
-H['m'] = H['m'].astype(str)
-
-# print("hello",H['m'])
-
-# figM = px.scatter_geo(H, lat="lat", lon="lon",
-#                      hover_name="pays", size='size',
-#                      projection="natural earth", color='m', color_discrete_sequence=["hotpink","green"'chocolate', 'blue', 'red','black'],labels={"m": "Magnitude"})
-
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-
-# colors = ['hotpink','green','chocolate', 'blue', 'red','black']
-
-# fig_show = go.Figure()
-
-# fig_pie = go.Pie(values=E['effectif'], labels=E["index"], marker=list(palette.values()))
-
-# fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20,
-#                   marker=dict(colors=colors, line=dict(color='#000000', width=2)))
-
-# fig_show.add_trace(fig_pie)
 
 import plotly.graph_objects as go
 
@@ -198,25 +160,11 @@ Q['m'] = Q['mag'].astype(int)
 Q['size'] = 5 + 2 * (Q['m'] - 2)
 Q = Q.sort_values(by='m',ascending = True)
 
-colors = ['hotpink','green','chocolate', 'blue', 'red','black']
-
-palettes = {
-    3: 'hotpink',
-    4: 'green',
-    5: 'chocolate',
-    6: 'blue',
-    7: 'red',
-    8: 'black'
-}
-
-# Création de la liste de couleurs en fonction des valeurs de m
-couleurs = [palettes[m] for m in Q['m']]
-
 fig_final = go.Figure()
     
 fig_pie = go.Pie(
-    labels=E["index"],
-    values=E['values'],
+    labels=E['index'],
+    values=E['effectif'],
     domain={'x': [0, 0.1], 'y': [0.8, 1]},
     marker_colors=list(palette.values()),
     legendgroup='magnitude',  # Assignation du même legendgroup pour le trace principal
@@ -233,14 +181,13 @@ for i in range(3, 9):
         mode='markers',
         marker=dict(
             size=Q[Q['m'] == i]['size'],
-            color=['hotpink', 'green', 'chocolate', 'blue', 'red', 'black'][i-3],
+            color=list(palette.values())[i-3],
             line=dict(width=0.5, color='white'),
         ),
-        hoverinfo='none',  # Pas de texte de survol
+        text = 'Région : '+Q['pays']+'<br>'+'Date : '+Q['instant']+'<br>'+'Profondeur (km) : '+Q['profondeur'].astype(str),
         name=str(i),
-        
     )
-    fig_final.add_trace(trace_temp)  # Ajout de la trace à la figure
+    fig_final.add_trace(trace_temp)
 
 fig_final.add_trace(fig_pie)
 
@@ -252,148 +199,21 @@ fig_final.update_layout(
     height=700,
     margin={"r": 0, "t": 0, "l": 0, "b": 0},
     dragmode=False,
-    title = 'Séisme en 2014 selon leur magnitude',
+    title = 'Séismes en 2014 selon leur magnitude',
     title_x=0.5,
     legend=dict(title="Magnitude")
 )
 
-#---------------------------------------------#
 fig_final.show()
-#---------------------------------------------#
-
-# fig_show.add_trace(
-#      go.Scattergeo(H, lat="lat", lon="lon",
-#                      hover_name="pays", size='size',
-#                      projection="natural earth", color='m', labels={"m": "Magnitude"},color_discrete_sequence=list(palette.values())),
-#     row=1, col=1
-# )
-
-# fig_show.update_layout(title='Séisme en 2014 selon leur magnitude', title_x=0.5)
-
-# fig3.show()
-
-# fig_show.add_trace(
-#      go.Pie(E, values='effectif', names="index", color_discrete_sequence=list(palette.values())),
-#      row=1, col=2
-# )
-
-# fig_pie.update_layout(
-#             title={
-#             'text' : 'Répartition',
-#             'x':0.5,
-#             'xanchor': 'center'
-#         })
-
-# fig_pie.show()
-
-# fig_show.update_layout(
-#     template="plotly_dark",
-#     margin=dict(r=10, t=25, b=40, l=60),
-#     annotations=[
-#         dict(
-#             text="Source: NOAA",
-#             showarrow=False,
-#             xref="paper",
-#             yref="paper",
-#             x=0,
-#             y=0)
-#     ]
-# )
-
-# fig_pie.update_traces(hole=0.4)
-
-# fig_show.show()
-
-#associate fig3 and fig4
-# fig3.add_trace(fig4.data[0])
-# fig3.show()
-
-# fig3.add_trace(fig4.data[0])
-
-# fig3.show()
 
 #---------------------------------------------#
 #                                             #
 #----------------- Partie IV -----------------#
 #                                             #
 #---------------------------------------------#
-# import plotly.graph_objects as go
-# from plotly.subplots import make_subplots
 
-# import pandas as pd
 
-# # read in volcano database data
-# df2 = pd.read_csv(
-#     "https://raw.githubusercontent.com/plotly/datasets/master/volcano_db.csv",
-#     encoding="iso-8859-1",
-# )
 
-# # frequency of Country
-# freq = df2
-# freq = freq.Country.value_counts().reset_index().rename(columns={"index": "x"})
-
-# # read in 3d volcano surface data
-# df_v = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/volcano.csv")
-
-# # Initialize figure with subplots
-# fig = make_subplots(
-#     rows=2, cols=2,
-#     column_widths=[0.6, 0.4],
-#     row_heights=[0.4, 0.6],
-#     specs=[[{"type": "scattergeo", "rowspan": 2}, {"type": "bar"}],
-#            [            None                    , {"type": "surface"}]])
-
-# # Add scattergeo globe map of volcano locations
-# fig.add_trace(
-#     go.Scattergeo(lat=df2["Latitude"],
-#                   lon=df2["Longitude"],
-#                   mode="markers",
-#                   hoverinfo="text",
-#                   showlegend=False,
-#                   marker=dict(color="crimson", size=4, opacity=0.8)),
-#     row=1, col=1
-# )
-
-# # Add locations bar chart
-# fig.add_trace(
-#     go.Bar(x=freq["x"][0:10],y=freq["Country"][0:10], marker=dict(color="crimson"), showlegend=False),
-#     row=1, col=2
-# )
-
-# # Add 3d surface of volcano
-# fig.add_trace(
-#     go.Surface(z=df_v.values.tolist(), showscale=False),
-#     row=2, col=2
-# )
-
-# # Update geo subplot properties
-# fig.update_geos(
-#     projection_type="orthographic",
-#     landcolor="white",
-#     oceancolor="MidnightBlue",
-#     showocean=True,
-#     lakecolor="LightBlue"
-# )
-
-# # Rotate x-axis labels
-# fig.update_xaxes(tickangle=45)
-
-# # Set theme, margin, and annotation in layout
-# fig.update_layout(
-#     template="plotly_dark",
-#     margin=dict(r=10, t=25, b=40, l=60),
-#     annotations=[
-#         dict(
-#             text="Source: NOAA",
-#             showarrow=False,
-#             xref="paper",
-#             yref="paper",
-#             x=0,
-#             y=0)
-#     ]
-# )
-
-# fig.show()
 #------------------ Notes ---------------------
 
 # instant = np.array(df['lat'])
