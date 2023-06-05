a141cc081c2a7c15ff10469dd6539016315ace6b
MaxenceRogerieux <71320557+MaxenceRogerieux@users.noreply.github.com>
2023-06-05 22:33:06 +0200

    01

--- projet.py
+++ projet.py
@@ -296,8 +296,8 @@ magNC = magNC.sort_index()
 print("magC : ",magC)
 print("magNC : ",magNC)
 
-magC['coastal'] = True
-magNC['coastal'] = False
+magC['coastal'] = 1
+magNC['coastal'] = 0
 
 mag = pd.concat([magC, magNC])
 
